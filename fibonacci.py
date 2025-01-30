ed6d968c4633a336df479104aeba1d32f4fe5d57
Mark Maslakowski <62085399+markmaz@users.noreply.github.com>
2025-01-30 15:15:51 -0600

    Create fibonacci.py

---
+++ fibonacci.py
@@ -0,0 +1,11 @@
+def fibonacci(n):
+    if n <= 0:
+        return "Invalid input. Enter a positive integer."
+    elif n == 1:
+        return [0]
+    elif n == 2:
+        return [0, 1]
+    else:
+        seq = fibonacci(n - 1)  # Recursive call
+        seq.append(seq[-1] + seq[-2])  # Add the next Fibonacci number
+        return seq
