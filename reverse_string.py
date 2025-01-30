3d473b45c16b5cce00804d233924164d73ace0a2
Mark Maslakowski <62085399+markmaz@users.noreply.github.com>
2025-01-30 15:13:54 -0600

    Create reverse_string.py

---
+++ reverse_string.py
@@ -0,0 +1,11 @@
+def reverse_string(s):
+    return s[::-1]
+
+def reverse_string(s):
+    reversed_s = ""
+    for char in s:
+        reversed_s = char + reversed_s
+    return reversed_s
+
+def reverse_string(s):
+    return "".join(reversed(s))
